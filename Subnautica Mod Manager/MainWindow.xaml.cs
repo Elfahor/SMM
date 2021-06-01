6de6afef3d36d1e1d3ac64c17bd8068b2f123d1c
elfah <elfah@ACER-E15-ELFAH>
2021-06-01 19:05:22 +0200

    Saving before DataGrid modif

--- Subnautica Mod Manager/MainWindow.xaml.cs
+++ Subnautica Mod Manager/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 using System.Windows;
 
@@ -13,7 +14,10 @@ namespace Subnautica_Mod_Manager
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public List<Mod> ModList = new List<Mod>();
+		public List<Mod> InstalledModList;
+		public List<Mod> LatestModList;
+		public List<Mod> PopularModList;
+		private readonly HttpClient httpClient = new HttpClient();
 
 		public MainWindow()
 		{
@@ -24,23 +28,117 @@ namespace Subnautica_Mod_Manager
 			SearchForLastVerBtn.Click += SearchForLastVerBtn_Click;
 			StartGameBtn.Click += StartGameBtn_Click;
 
+			httpClient.BaseAddress = new Uri("https://api.nexusmods.com/v1/games/subnautica/");
+			httpClient.DefaultRequestHeaders.Add("apikey", Properties.Settings.Default.NexusApiKey);
 
-			string[] installedMods = Directory.GetDirectories(Properties.Settings.Default.GamePath + "\\QMods", "*", SearchOption.TopDirectoryOnly);
+			InstalledModList = GetMods(ModsToShow.ShowInstalled);
+			LatestModList = GetMods(ModsToShow.ShowLatest);
+			PopularModList = GetMods(ModsToShow.ShowPopular);
 
-			foreach (string mod in installedMods)
+			string[] listViewModes = Enum.GetNames(typeof(ModsToShow));
+			ShowWhatComboBox.ItemsSource = listViewModes;
+			ShowWhatComboBox.SelectedIndex = 0;
+
+			ShowWhatComboBox.SelectionChanged += (sender, e) =>
 			{
-				if (Path.GetFileName(mod) != ".backups")
+				ModsToShow selectedItem = (ModsToShow)ShowWhatComboBox.SelectedIndex;
+				switch (selectedItem)
 				{
-					ModList.Add(new Mod(mod));
+					case ModsToShow.ShowInstalled:
+						InstalledModsListControl.Visibility = Visibility.Visible;
+						OnlineModListControl.Visibility = Visibility.Collapsed;
+						break;
+					case ModsToShow.ShowLatest:
+						InstalledModsListControl.Visibility = Visibility.Collapsed;
+						OnlineModListControl.Visibility = Visibility.Visible;
+						OnlineModListControl.ItemsSource = LatestModList;
+						break;
+					case ModsToShow.ShowPopular:
+						InstalledModsListControl.Visibility = Visibility.Collapsed;
+						OnlineModListControl.Visibility = Visibility.Visible;
+						OnlineModListControl.ItemsSource = PopularModList;
+						break;
+				}
+			};
+
+			InstalledModsListControl.ItemsSource = InstalledModList;
+			OnlineModListControl.ItemsSource = PopularModList;
+		}
+
+		private enum ModsToShow
+		{
+			ShowInstalled,
+			ShowLatest,
+			ShowPopular
+		}
+
+		private bool DependenciesOk() =>
+			InstalledModList.All((m) =>
+				m.ModJson.Dependencies.All((d) =>
+					InstalledModList.Any((mc) => mc.ModJson.Id == d
+					)
+				)
+			);
+
+		private List<Mod> GetMods(ModsToShow modsToShow)
+		{
+			List<Mod> mods = new List<Mod>();
+
+			if (modsToShow == ModsToShow.ShowInstalled)
+			{
+				string[] installedMods = Directory.GetDirectories(Properties.Settings.Default.GamePath + "\\QMods", "*", SearchOption.TopDirectoryOnly);
+
+				foreach (string mod in installedMods)
+				{
+					if (Path.GetFileName(mod) != ".backups")
+					{
+						mods.Add(new Mod(mod));
+					}
+				}
+			}
+			else if (modsToShow == ModsToShow.ShowLatest)
+			{
+				HttpResponseMessage response = httpClient.GetAsync("mods/latest_added.json").Result;
+				List<Mod.DownloadedModData> downloadedModData = JsonSerializer.Deserialize<List<Mod.DownloadedModData>>(response.Content.ReadAsStringAsync().Result);
+				foreach (Mod.DownloadedModData item in downloadedModData)
+				{
+					mods.Add(new Mod(item));
+				}
+			}
+			else if (modsToShow == ModsToShow.ShowPopular)
+			{
+				HttpResponseMessage response = httpClient.GetAsync("mods/trending.json").Result;
+				List<Mod.DownloadedModData> downloadedModData = JsonSerializer.Deserialize<List<Mod.DownloadedModData>>(response.Content.ReadAsStringAsync().Result);
+				foreach (Mod.DownloadedModData item in downloadedModData)
+				{
+					mods.Add(new Mod(item));
 				}
 			}
 
-			modListControl.ItemsSource = ModList;
+			return mods;
+		}
+
+		private void ApplyModifsBtn_Click(object sender, RoutedEventArgs e)
+		{
+			foreach (Mod mod in InstalledModList)
+			{
+				mod.ApplyModJson();
+			}
+		}
+
+		private void OpenSettingsBtn_Click(object sender, RoutedEventArgs e)
+		{
+			SettingsDialog settingsDialog = new SettingsDialog()
+			{
+				Owner = this,
+				DataContext = this.DataContext
+			};
+			settingsDialog.ShowDialog();
 		}
 
 		private void SearchForLastVerBtn_Click(object sender, RoutedEventArgs e)
 		{
-			foreach (Mod mod in ModList)
+			foreach (Mod mod in InstalledModList)
 			{
 				int modId = int.Parse(mod.ModJson.NexusId.Subnautica);
 			}
@@ -56,32 +154,6 @@ namespace Subnautica_Mod_Manager
 			using Process game = new Process();
 			game.StartInfo.FileName = Path.Combine(Properties.Settings.Default.GamePath, "Subnautica.exe");
 			game.Start();
-		}
-
-		private bool DependenciesOk() =>
-			ModList.All((m) =>
-				m.ModJson.Dependencies.All((d) =>
-					ModList.Any((mc) => mc.ModJson.Id == d
-					)
-				)
-			);
-
-		private void ApplyModifsBtn_Click(object sender, RoutedEventArgs e)
-		{
-			foreach (Mod mod in ModList)
-			{
-				mod.ApplyModJson();
-			}
-		}
-
-		private void OpenSettingsBtn_Click(object sender, RoutedEventArgs e)
-		{
-			SettingsDialog settingsDialog = new SettingsDialog()
-			{
-				Owner = this,
-				DataContext = this.DataContext
-			};
-			settingsDialog.ShowDialog();
 		}
 	}
 }
--- Subnautica Mod Manager/Mod.cs
+++ Subnautica Mod Manager/Mod.cs
@@ -16,26 +16,27 @@ namespace Subnautica_Mod_Manager
 			GetFromJson();
 		}
 
-		private void GetFromJson()
+		public Mod()
 		{
-			if (File.Exists(ModJsonPath))
-			{
-				ModJson = JsonSerializer.Deserialize<ModJsonFile>(File.ReadAllText(ModJsonPath));
-				ModJson.Dependencies = ModJson.Dependencies ?? new string[0];
-			}
-			else
-			{
-				throw new ArgumentException($"{ModJsonPath} does not exist");
-			}
+
 		}
 
-		public string Name { get; set; }
+		public Mod(DownloadedModData onlineInfo)
+		{
+			OnlineInfo = onlineInfo;
+			Name = OnlineInfo.name;
+			LastVersion = onlineInfo.version;
+		}
 
-		public string ModJsonPath { get; }
+		public string LastVersion { get; set; }
 
 		public ModJsonFile ModJson { get; private set; }
 
-		public string LastVersion { get; set; }
+		public DownloadedModData OnlineInfo { get; private set; }
+
+		public string ModJsonPath { get; }
+
+		public string Name { get; set; }
 
 		public override bool Equals(object obj)
 		{
@@ -52,22 +53,73 @@ namespace Subnautica_Mod_Manager
 			return Name;
 		}
 
+		internal void ApplyModJson()
+		{
+			string newContent = JsonSerializer.Serialize(ModJson, JsonSerializerOptions);
+			File.WriteAllText(ModJsonPath, newContent);
+			GetFromJson();
+		}
+
+		private void GetFromJson()
+		{
+			if (File.Exists(ModJsonPath))
+			{
+				ModJson = JsonSerializer.Deserialize<ModJsonFile>(File.ReadAllText(ModJsonPath));
+				ModJson.Dependencies = ModJson.Dependencies ?? new string[0];
+			}
+			else
+			{
+				throw new ArgumentException($"{ModJsonPath} does not exist");
+			}
+		}
+		public class DownloadedModData
+		{
+			public bool allow_rating { get; set; }
+			public string author { get; set; }
+			public bool available { get; set; }
+			public int category_id { get; set; }
+			public bool contains_adult_content { get; set; }
+			public DateTime created_time { get; set; }
+			public int created_timestamp { get; set; }
+			public string description { get; set; }
+			public string domain_name { get; set; }
+			public object endorsement { get; set; }
+			public int endorsement_count { get; set; }
+			public int game_id { get; set; }
+			public int mod_id { get; set; }
+			public string name { get; set; }
+			public string picture_url { get; set; }
+			public string status { get; set; }
+			public string summary { get; set; }
+			public long uid { get; set; }
+			public DateTime updated_time { get; set; }
+			public int updated_timestamp { get; set; }
+			public string uploaded_by { get; set; }
+			public string uploaded_users_profile_url { get; set; }
+			public User user { get; set; }
+			public string version { get; set; }
+			public class User
+			{
+				public int member_group_id { get; set; }
+				public int member_id { get; set; }
+				public string name { get; set; }
+			}
+		}
 
 		public class ModJsonFile
 		{
-			public string Id { get; set; }
-			public string DisplayName { get; set; }
-			public string Author { get; set; }
-			public string Version { get; set; }
-			public string[] Dependencies { get; set; }
-			public string[] LoadBefore { get; set; }
-			public bool Enable { get; set; }
-			public string Game { get; set; }
-			public Nexusid NexusId { get; set; }
-			public Versionchecker VersionChecker { get; set; }
 			public string AssemblyName { get; set; }
+			public string Author { get; set; }
+			public string[] Dependencies { get; set; }
+			public string DisplayName { get; set; }
+			public bool Enable { get; set; }
 			public string EntryMethod { get; set; }
-
+			public string Game { get; set; }
+			public string Id { get; set; }
+			public string[] LoadBefore { get; set; }
+			public Nexusid NexusId { get; set; }
+			public string Version { get; set; }
+			public Versionchecker VersionChecker { get; set; }
 			public struct Nexusid
 			{
 				public string Subnautica { get; set; }
@@ -77,13 +129,6 @@ namespace Subnautica_Mod_Manager
 			{
 				public string LatestVersionURL { get; set; }
 			}
-		}
-
-		internal void ApplyModJson()
-		{
-			string newContent = JsonSerializer.Serialize(ModJson, JsonSerializerOptions);
-			File.WriteAllText(ModJsonPath, newContent);
-			GetFromJson();
 		}
 	}
 }
